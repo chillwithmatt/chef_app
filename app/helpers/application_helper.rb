cc62d8aef80341c244ead993a070dbb80826be7e
matt <mtl858@gmail.com>
2013-12-25 21:34:28 -0800

    popup fb bug mobile

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,14 @@
 module ApplicationHelper
 	def link_to_login_with(provider, url, popup = {})
-    	link_to t('.sign_up_with_facebook', provider: provider), url,
-      	class: 'js-popup btn btn-primary btn-lg', 
-      	data: { width: popup[:width], height: popup[:height], 
-      	onclick: "ga('send', 'event', 'fb_signup', 'click');" }
+		if session[:mobile_param]
+			link_to "Sign up with Facebook", "/auth/facebook", 
+				id: "sign_in", class: "btn btn-primary btn-lg", 
+				onclick: "ga('send', 'event', 'fb_signup', 'click');"
+		else
+    		link_to t('.sign_up_with_facebook', provider: provider), url,
+      			class: 'js-popup btn btn-primary btn-lg', 
+      			data: { width: popup[:width], height: popup[:height], 
+      			onclick: "ga('send', 'event', 'fb_signup', 'click');" }
+      	end
   	end
 end
