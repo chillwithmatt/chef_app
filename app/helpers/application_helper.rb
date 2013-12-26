2acba3fd08817d80c79769e47ab66bf60104479d
matt <mtl858@gmail.com>
2013-12-26 12:26:39 -0800

    onclick bug

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,7 @@
 module ApplicationHelper
 	def link_to_login_with(provider, url, popup = {})
-    	link_to t('.sign_up_with_facebook', provider: provider), url,
-      	class: 'js-popup btn btn-primary btn-lg', 
-      	data: { width: popup[:width], height: popup[:height], 
-      	onclick: "ga('send', 'event', 'fb_signup', 'click');" }
+    	link_to t('.sign_up_with_facebook', provider: provider, onclick: "ga('send', 'event', 'fb_signup', 'click');"), 
+    	url, class: 'js-popup btn btn-primary btn-lg', 
+      	data: { width: popup[:width], height: popup[:height] }
   	end
 end
