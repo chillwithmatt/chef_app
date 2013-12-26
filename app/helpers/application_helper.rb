9f21eaaf874c914f3b1b008a7e61999bc7720dd8
matt <mtl858@gmail.com>
2013-12-26 15:18:42 -0800

    ga bug

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
 	def link_to_login_with(provider, url, popup = {})
-    	link_to t('.sign_up_with_facebook', provider: provider, onclick: "ga('send', 'event', 'fb_signup', 'click');"), 
+    	link_to t('.sign_up_with_facebook', provider: provider), 
     	url, class: 'js-popup btn btn-primary btn-lg', 
       	data: { width: popup[:width], height: popup[:height] }
   	end
