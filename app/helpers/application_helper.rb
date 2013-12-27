996f15131f6a7c55856054d4963620ff474b0c55
matt <mtl858@gmail.com>
2013-12-26 18:37:37 -0800

    connect w fb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 	def link_to_login_with(provider, url, popup = {})
     	link_to t('connect with_facebook', provider: provider), 
     	url, class: 'js-popup btn btn-primary btn-lg', 
-    	onclick: "ga('send', 'event', 'fb_signup', 'click');",
+    	onclick: "ga('send', 'event', 'fb_connect', 'click');",
       	data: { width: popup[:width], height: popup[:height] }
   	end
 end
