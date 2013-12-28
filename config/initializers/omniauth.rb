8d93b3edbf5c2ad482e47131d0ebfad774970a62
matt <mtl858@gmail.com>
2013-12-27 22:39:41 -0800

    hp dishes new tab

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, FACEBOOK_CONFIG['app_id'], FACEBOOK_CONFIG['secret'], scope: "email, user_location", display: "popup"
+  provider :facebook, FACEBOOK_CONFIG['app_id'], FACEBOOK_CONFIG['secret'], scope: "email", display: "popup"
 end
 
 #publish_actions
