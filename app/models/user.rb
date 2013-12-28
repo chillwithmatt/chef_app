c155250913be0f07dff0f0b08ef2ed7cd671fb91
matt <mtl858@gmail.com>
2013-12-27 22:52:33 -0800

    removed location validation/permissions

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ActiveRecord::Base
   	validates :email, presence: true, 
   			  format: { with: VALID_EMAIL_REGEX }, 
   			  uniqueness: { case_sensitive: false }
-  	validates :city, presence: true
+  	#validates :city, presence: true
 
 	has_secure_password
 
