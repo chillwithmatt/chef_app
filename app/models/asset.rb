d2cc10eda9e2ebd32578205f3e5bb8d0779adf97
matt <mtl858@gmail.com>
2014-01-18 00:24:26 -0800

    new medium asset size

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -4,8 +4,8 @@ class Asset < ActiveRecord::Base
 	has_attached_file :photo, 
 		:styles => {
 			:thumb => '100x100#',
-			:medium => '200x200>',
-			:large => '600x600>'
+			:medium => '150x150#',
+			:large => '600x600#'
 		}
 
 end
